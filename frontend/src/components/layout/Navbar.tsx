4833cbcd5c57853dc10b1a6d3c06052403281b3d
oskar-wichtowski-wttech <oskar.wichtowski@wundermanthompson.com>
2025-06-14 15:41:38 +0200

    FEAT 10 - change navbar behaviour

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -2,16 +2,33 @@
 import React from 'react';
 import Link from 'next/link';
 import { useUser } from '../context/UserContext';
+import { usePathname } from 'next/navigation';
+
+const HomeLink = () => (
+  <Link href="/" className="text-2xl font-bold text-primary-lightest">
+    ShortenMe
+  </Link>
+);
 
 const Navbar = () => {
   const { user } = useUser();
+  const pathname = usePathname();
+  const isRedirectPage = pathname?.startsWith('/r/');
+
+  if (isRedirectPage) {
+    return (
+      <nav className="fixed top-0 left-0 right-0 z-50 bg-primary-dark/50 backdrop-blur-sm border-b border-primary-light/20">
+      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+        <HomeLink />
+      </div>
+    </nav>
+  );
+  }
 
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 bg-primary-dark/50 backdrop-blur-sm border-b border-primary-light/20">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-        <Link href="/" className="text-2xl font-bold text-primary-lightest">
-          ShortenMe
-        </Link>
+        <HomeLink />
         <div className="space-x-4">
           {user ? (
             <Link
